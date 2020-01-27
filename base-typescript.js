3eb9ee6c224276e55e402b708d6ae63869099f83
edgar0011 <martin.weiser@gmail.com>
2020-01-27 10:34:17 +0100

    Updates due to new babel, TS, JSX

--- base-typescript.js
+++ base-typescript.js
@@ -1,54 +1,66 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "extends": [
-    "standard",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+  'parser': '@typescript-eslint/parser',
+  'extends': [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
   ],
-  "plugins": ["react", "@typescript-eslint", "standard"],
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
+  'plugins': ['@typescript-eslint', 'standard'],
+  'parserOptions': {
+    'sourceType': 'module',
+  },
+  'env': {
+    'es6': true,
+    'node': true,
+  },
+  'rules': {
+    'generator-star-spacing': ['error', 'after'],
+    'max-len': ['error', { 'code': 120 }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': ['error', 'never'],
+    'operator-linebreak': ['error', 'before'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', { 'multiline': { 'delimiter': 'none' } }],
+    '@typescript-eslint/prefer-interface': 0,
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    // already used via esling plugin, this ignores JSX usage...
+    // https://github.com/typescript-eslint/typescript-eslint/issues/868
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'import/named': 'warn',
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never",
+      },
+    ],
+  },
+  'globals': {
+    'describe': true,
+    'test': true,
+    'expect': true,
+    'it': true,
+    'cy': true,
+    'beforeEach': true,
+    'context': true,
+    'jest': true,
+    'fetch': true,
+  },
+  'settings': {
+    'react': {
+      'version': '16.7',
     },
-  },
-  "env": {
-    "es6": true,
-    "node": true,
-  },
-  "rules": {
-    "generator-star-spacing": ["error", "after"],
-    "max-len": ["error", { "code": 120 }],
-    "comma-dangle": ["error", "always-multiline"],
-    "space-before-function-paren": ["error", "never"],
-    "operator-linebreak": ["error", "before"],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/member-delimiter-style": ["error", { "multiline": { "delimiter": "none" } }],
-    "@typescript-eslint/prefer-interface": 0,
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "react/display-name": "off",
-    "react/prop-types": "off",
-    "import/named": "warn",
-
-  },
-  "globals": {
-    "describe": true,
-    "test": true,
-    "expect": true,
-    "it": true,
-    "cy": true,
-    "beforeEach": true,
-    "context": true,
-    "jest": true,
-    "fetch": true,
-  },
-  "settings": {
-    "react": {
-      "version": "16.7",
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx", ".json"],
+      },
     },
   },
 }
--- base.js
+++ base.js
@@ -1,62 +1,61 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "airbnb",
-    "eslint:recommended",
-    "plugin:flowtype/recommended",
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'eslint:recommended',
   ],
-  "plugins": [
-    "babel",
-    "flowtype",
-    "promise",
-    "eslint-plugin-import-order",
-    "eslint-plugin-import-order-alphabetical",
+  plugins: [
+    'babel',
+    'promise',
+    'eslint-plugin-import-order',
+    'eslint-plugin-import-order-alphabetical',
   ],
-  "env": {
-    "browser": true,
-    "jest": true,
+  env: {
+    browser: true,
+    jest: true,
   },
-  "globals": {
-    "describe": true,
-    "test": true,
-    "expect": true,
-    "it": true,
+  globals: {
+    describe: true,
+    test: true,
+    expect: true,
+    it: true,
   },
-  "rules": {
-    "arrow-parens": ["error", "always"],
-    "import-order-alphabetical/order": "off",
-    "import/order": ["error", { "newlines-between": "always" }],
-    "import/prefer-default-export": "off",
-    "no-shadow": "off",
-    "no-unused-expressions": "off",
-    "no-unused-vars": "error",
-    "no-constant-condition": "off",
-    "no-useless-constructor": "warn",
-    "no-multi-spaces": "error",
-    "comma-dangle": [
-      "error",
-      "always-multiline",
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'implicit-arrow-linebreak': ['error', 'beside'],
+    'import-order-alphabetical/order': 'off',
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': 'error',
+    'no-constant-condition': 'off',
+    'no-useless-constructor': 'warn',
+    'no-multi-spaces': 'error',
+    'comma-dangle': [
+      'error',
+      'always-multiline',
     ],
-    "quotes": ["error", "single"],
-    "max-len": [
-      "error",
+    quotes: ['error', 'single'],
+    'max-len': [
+      'error',
       120,
       2,
     ],
-    "object-curly-spacing": [
-      "error",
-      "always",
+    'object-curly-spacing': [
+      'error',
+      'always',
     ],
-    "semi": [
-      "error",
-      "never",
+    semi: [
+      'error',
+      'never',
     ],
 
-    "object-curly-newline": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
+    'object-curly-newline': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        "devDependencies": true,
+        devDependencies: true,
       },
     ],
   },
