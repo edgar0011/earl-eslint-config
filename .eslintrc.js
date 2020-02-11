793c7ec22b59d73ebd000e6038a854778a6cd867
edgar0011 <martin.weiser@gmail.com>
2020-02-11 12:44:14 +0100

    Updates deps - adds eslintrc just for local eslint run

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: './base.js',
+  extends: './base-react-jsx.js',
+  extends: './base-typescript.js',
+  extends: './perf-react-jsx.js',
+
+  rules: {
+    "quotes": "off",
+  }
+};
